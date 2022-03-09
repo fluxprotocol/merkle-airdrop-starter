5200e9a37aa385e8353889cb5a77a80410c8bc41
Jameson Hodge <contact@jamesonhodge.com>
2022-03-09 07:49:52 -0500

    feat: forward tokens instead of minting new ones

--- contracts_hardhat/test/merkleAirdrop/MerkleAirdrop.behaviour.ts
+++ contracts_hardhat/test/merkleAirdrop/MerkleAirdrop.behaviour.ts
@@ -4,17 +4,9 @@ import { getAddress, keccak256 } from "ethers/lib/utils";
 import { TEST_VALUE } from "../types";
 
 export function shouldBehaveLikeMerkleAirdrop(): void {
-  
-
   it("should let user1 claim tokens", async function () {
-    
-   
-    // console.log("solidityPack = ", utils.solidityPack([ "address", "uint256" ], [ getAddress("0x5513D4Efd95A19aeF075278d4189D042332aF440"), utils.parseEther("1000") ]))
-    // let proof = utils.solidityKeccak256([ "address", "uint256" ], [ getAddress("0x5513D4Efd95A19aeF075278d4189D042332aF440"), utils.parseEther("1000") ])
-    // user1Proof[0] = proof;
-    // console.log("solidityKeccak256 = ", user1Proof[0]);
     // Collect user1 balance of tokens before claim
-    let user1PreBalance = await this.merkleClaimERC20.connect(this.signers.admin).balanceOf(this.user1Address);
+    let user1PreBalance = await this.erc20Faucet.connect(this.signers.admin).balanceOf(this.user1Address);
     console.log("user1PreBalance", user1PreBalance);
     // Claim tokens
     await this.merkleClaimERC20.connect(this.signers.admin).claim(
@@ -23,71 +15,83 @@ export function shouldBehaveLikeMerkleAirdrop(): void {
       // 1000 tokens
       utils.parseEther("10000"),
       // With valid proof
-      this.user1CorrectProof
+      this.user1CorrectProof,
     );
 
     // Collect user1 balance of tokens after claim
-    let user1PostBalance = await this.merkleClaimERC20.connect(this.signers.admin).balanceOf(this.user1Address);
+    let user1PostBalance = await this.erc20Faucet.connect(this.signers.admin).balanceOf(this.user1Address);
     console.log("user1PostBalance", user1PostBalance);
 
-    expect(user1PostBalance).to.be.equal(user1PreBalance + utils.parseEther("10000"))
+    expect(user1PostBalance).to.be.equal(user1PreBalance.add(utils.parseEther("10000")));
   });
 
-
   it("should revert wrong proof", async function () {
-
-    try{
-
+    try {
       await this.merkleClaimERC20.connect(this.signers.admin).claim(
         // Claiming for user1
         this.user1Address,
         // 1000 tokens
         utils.parseEther("10000"),
         // With valid proof
-        this.user1WrongProof
+        this.user1WrongProof,
       );
-      
-
-    }catch(e){
+    } catch (e) {
       if (!(e instanceof Error)) return;
       expect(e.message).to.include(
         "VM Exception while processing transaction: reverted with custom error 'NotInMerkle()'",
       );
-
     }
-  
-  
   });
 
   it("should revert user2 ", async function () {
-    
-   
-  
-    try{
-
+    try {
       await this.merkleClaimERC20.connect(this.signers.admin).claim(
         // Claiming for user1
         this.user2Address,
         // 1000 tokens
         utils.parseEther("10000"),
         // With valid proof
-        this.user1CorrectProof
+        this.user1CorrectProof,
       );
-  
-      
-
-    }catch(e){
+    } catch (e) {
       if (!(e instanceof Error)) return;
       expect(e.message).to.include(
         "VM Exception while processing transaction: reverted with custom error 'NotInMerkle()'",
       );
-
     }
-  
   });
 
+  it("should let admin forward (stuck) erc20s", async function () {
+    // Collect admin balance of tokens before claim
+    let adminPreBalance = await this.erc20Faucet.connect(this.signers.admin).balanceOf(this.signers.admin.address);
+    console.log("adminPreBalance", adminPreBalance);
 
+    // forward erc20s
+    await this.merkleClaimERC20.connect(this.signers.admin).forwardERC20s(
+      this.erc20Faucet.address, // FLX token
+      utils.parseEther("123"),
+    );
 
-  
- 
+    // Collect admin balance of tokens after claim
+    let adminPostBalance = await this.erc20Faucet.connect(this.signers.admin).balanceOf(this.signers.admin.address);
+    console.log("adminPostBalance", adminPostBalance);
+
+    expect(adminPostBalance).to.be.equal(adminPreBalance.add(utils.parseEther("123")));
+  });
+
+  it("should not let users forward (stuck) erc20s", async function () {
+    try {
+      // forward erc20s
+      await this.merkleClaimERC20.connect(this.signers.nonadmin).forwardERC20s(
+        this.erc20Faucet.address, // FLX token
+        utils.parseEther("123"),
+      );
+    } catch (e) {
+      if (!(e instanceof Error)) return;
+      console.log(e.message);
+      expect(e.message).to.include(
+        "VM Exception while processing transaction: reverted with reason string 'Ownable: caller is not the owner'",
+      );
+    }
+  });
 }
--- contracts_hardhat/test/types.ts
+++ contracts_hardhat/test/types.ts
@@ -2,12 +2,14 @@ import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signe
 import type { Fixture } from "ethereum-waffle";
 
 import type { MerkleClaimERC20 } from "../src/types/MerkleClaimERC20";
+import type { ERC20Faucet } from "../src/types/ERC20Faucet";
 
 export const TEST_VALUE = 12345;
 
 declare module "mocha" {
   export interface Context {
     merkleClainERC20: MerkleClaimERC20;
+    erc20Faucet: ERC20Faucet;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
@@ -15,6 +17,7 @@ declare module "mocha" {
 
 export interface Signers {
   admin: SignerWithAddress;
+  nonadmin: SignerWithAddress;
   // alice: SignerWithAddress;
   // bob: SignerWithAddress;
 }
